3781fc28497196986b842572c3bd6b128f0ffb6b
Eduarda Trindade :) <eduarda.trindade@escolar.ifrn.edu.br>
2025-09-22 14:28:29 -0300

    Aula 04/1

--- Projeto 01 - 3bim/Assets/codigos/Inimigo.cs
+++ Projeto 01 - 3bim/Assets/codigos/Inimigo.cs
@@ -1,41 +1,44 @@
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class Inimigo : Personagem
 {
     [SerializeField] private int dano = 1;
 
-    public void setdano(int dano)
+    public void setDano(int dano)
     {
         this.dano = dano;
     }
-
-    public int getdano()
+    public int getDano()
     {
         return this.dano;
     }
-   void Start()
-   {
-      
-   }
 
-   void Update()
-   {
-       if (getVidas() <= 0)
-       {
-           //desativa o objeto Inimigo
-           gameObject.SetActive(false);
-       }   
-   }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (getVidas() <= 0)
+        {
+            //desativa o objeto do Inimigo
+            gameObject.SetActive(false);
+        }
+    }
 
     void OnCollisionEnter2D(Collision2D collision)
-   {
-       if (collision.gameObject.tag == "Player")
-       {
-             // Causa dano ao Player
-             int novaVida = collision.gameObject.GetComponent<Personagem>().getVidas() - getdano();
-             collision.gameObject.GetComponent<Personagem>().setVidas(novaVida); 
-             print ("A Vida restante do Player é " + novaVida);
-       }
-   }
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Causa dano ao Player
+            int novaVida = collision.gameObject.GetComponent<Personagem>().getVidas() - getDano();
+            collision.gameObject.GetComponent<Personagem>().setVidas(novaVida);
+
+            //collision.gameObject.GetComponent<Personagem>().recebeDano(getDano());
+            
+            //desativa quando bate no player
+            gameObject.SetActive(false);
+        }
+    }
 }
